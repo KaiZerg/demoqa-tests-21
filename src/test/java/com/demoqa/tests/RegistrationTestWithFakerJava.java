dbd0a8483eb10e71ded909b7511512d27a0493f5
KaiZerg <gabitov.tatarin@gmail.com>
2023-08-27 09:04:23 +0500

    added RemoteTestBase, Attach

--- src/test/java/com/demoqa/tests/RegistrationTestWithFakerJava.java
+++ src/test/java/com/demoqa/tests/RegistrationTestWithFakerJava.java
@@ -3,35 +3,42 @@ package com.demoqa.tests;
 import com.demoqa.pages.RegistrationPage;
 import com.demoqa.utils.TestDataProperties;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class RegistrationTestWithFakerJava extends TestBase {
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+public class RegistrationTestWithFakerJava extends RemoteTestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     TestDataProperties testData = new TestDataProperties();
 
     @Test
     @Tag("remote")
+    @DisplayName("Успешная регистрация при сабмите в форму авторизации - с генерацией данных")
     void registrationTest() {
-
-        registrationPage.openPage()
-                .removeFixedBanner()
-                .removeFooter()
-                .setFirstName(testData.firstNameValue)
-                .setLastName(testData.lastNameValue)
-                .setUserEmail(testData.emailValue)
-                .setGender(testData.genderValue)
-                .setUserNumber(testData.phoneNumber)
-                .setBirthDay(testData.monthValue, testData.yearValue, testData.dayValue)
-                .setSubjects(testData.subjectValue)
-                .setHobbies(testData.hobieValue)
-                .uploadPicture(testData.fileNameValue)
-                .setAddress(testData.addressValue)
-                .setState(testData.stateValue)
-                .setCity(testData.cityValue)
-                .clickOnSubmitButton();
-
+        step("Открываем страницу с формой авторизации", () -> {
+            registrationPage.openPage()
+                    .removeFixedBanner()
+                    .removeFooter();
+        });
+        step("Заполняем форму", () -> {
+            registrationPage.setFirstName(testData.firstNameValue)
+                    .setLastName(testData.lastNameValue)
+                    .setUserEmail(testData.emailValue)
+                    .setGender(testData.genderValue)
+                    .setUserNumber(testData.phoneNumber)
+                    .setBirthDay(testData.monthValue, testData.yearValue, testData.dayValue)
+                    .setSubjects(testData.subjectValue)
+                    .setHobbies(testData.hobieValue)
+                    .uploadPicture(testData.fileNameValue)
+                    .setAddress(testData.addressValue)
+                    .setState(testData.stateValue)
+                    .setCity(testData.cityValue);
+        });
+        step("Делаем сабмит", () -> registrationPage.clickOnSubmitButton());
+        step("Проверяем сохраненные данные", () -> {
         registrationPage.checkOverResults("Student Name", testData.firstNameValue + " " + testData.lastNameValue)
                 .checkOverResults("Student Email", testData.emailValue)
                 .checkOverResults("Gender", testData.genderValue)
@@ -42,5 +49,6 @@ public class RegistrationTestWithFakerJava extends TestBase {
                 .checkOverResults("Picture", testData.fileNameValue)
                 .checkOverResults("Address", testData.addressValue)
                 .checkOverResults("State and City", testData.stateValue + " " + testData.cityValue);
+        });
     }
 }
---
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -0,0 +1,52 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static org.asynchttpclient.util.HttpConstants.Methods.OPTIONS;
+
+public class RemoteTestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(OPTIONS);
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", true);
+        capabilities.setCapability("selenoid:options", selenoidOptions);
+        Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addVideo() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
+    }
+}

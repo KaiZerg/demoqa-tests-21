8cf7c408c5a3efe88575732fd010819b05542b9d
KaiZerg <gabitov.tatarin@gmail.com>
2023-08-29 15:44:02 +0500

    Added RemoteTestBase

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -4,9 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -17,10 +15,27 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static org.asynchttpclient.util.HttpConstants.Methods.OPTIONS;
 
 public class RemoteTestBase {
+    @Test
+    @Tag("remote")
+    void systemPropertiesTest() {
+        String browser = System.getProperty("browser", "mozilla");
+    }
+    @Test
+    @Tag("remote")
+    void testBrowserSize() {
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+    }
+
+    @Test
+    @Tag("remote")
+    void testBrowserVersion() {
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+    }
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.remote = System.getProperty("remoteDriverUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
